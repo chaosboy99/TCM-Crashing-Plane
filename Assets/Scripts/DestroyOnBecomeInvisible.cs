8e68ce48e8c532ad9394cd944a60a9c9f1b96222
chaosboy99 <brayanguanangamorales@gmail.com>
2024-04-04 16:23:42 +0200

    coin/mov

--- Assets/Scripts/DestroyOnBecomeInvisible.cs
+++ Assets/Scripts/DestroyOnBecomeInvisible.cs
@@ -6,4 +6,8 @@ private void OnTriggerEnter2D (Collider2D other)
     {
         Destroy(gameObject);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
