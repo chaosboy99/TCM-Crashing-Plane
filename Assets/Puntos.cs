2a304b791c93bd1e9eec108077df0b37fe700775
chaosboy99 <brayanguanangamorales@gmail.com>
2024-04-04 16:52:41 +0200

    ScripPuntos

---
+++ Assets/Puntos.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Puntos : MonoBehaviour
+{
+    private float puntos;
+    private TextMeshProUGUI TextMesh; 
+    
+    private void Start ()
+    {
+        
+    }
+    private void Update()
+    {
+
+    }
+
+}
